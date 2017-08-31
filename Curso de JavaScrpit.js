542663ef1b1fc35f3e1a7211ab12a81b4cf0222e
YorgeManuel <yorgemedina2017@gmail.com>
2017-08-31 15:40:07 -0400

    Tipos de Datos

--- Curso de JavaScrpit.js
+++ Curso de JavaScrpit.js
@@ -1,8 +1,52 @@
+//Tipos de datos 
 
-//estos tambien sirven para escribir tecto en pantalla 
+//numerico
 
-document.querySelector("body").innerHTML = "Hola Mundo";
+var numero = 2 , //entero
+    numero2= 23.4; //decimal
 
-//cuando el js esta vinculado con html utilisamos esta 
+//String o Cadenas de Texto
 
-document.write("Hola Mundo");
+var nombre = "Pedro";
+var texto = "el caballo blanco \"texto entre comillas\" de simon bolivar"
+
+/*No se pueden poner comillas dobles dentro de otras comillas dobles en el codigo
+de debe poner comillas simples '' o escapar el caracter con \" \" como esta en el
+ejemplo de arriba*/
+
+//Array
+//Permite almacenar muchos elementos pueden ser de valor numerico o tipo string
+
+var colores = ["Azul","Amarillo","Rojo","Verde"]
+document.write(colores[1]);
+
+/*para acceder a un valor de un array se coloca el nombre de la variable y luego del
+elemento contado desde el primero como 0*/
+
+//Objetos 
+
+var objetos={
+	color = "black";
+	size = 12;
+}
+
+//boolean o boleano 
+
+True 
+False
+
+//Valor indefinido
+
+Undefined
+
+//Para colocar el valor de una variable a nulo
+
+null
+
+//cuando colocas un valor invalido 
+
+//ejemplo 
+var tipo = 2 * dog ;
+
+NaN
+
